79f8d10eb5b8d581bd668836d681a6bcea75422c
E~ <elle.dot2019@gmail.com>
2020-05-15 01:09:59 +0100

    Pictures, fades and JavaScript
    EASY Grows, some accessibility adjustments for tiny screens and touch devices but incomplete. Picture fading is super cool however! :+1:

---
+++ JS/index.js
@@ -0,0 +1,35 @@
+function openElle() {
+	window.open("https://github.com/elledot", "_blank");
+}
+			
+function imageEOn() {
+	document.getElementById("imageE").style.opacity = "0.5";
+}
+			
+function imageEOff() {
+	document.getElementById("imageE").style.opacity = "0";
+}
+			
+function imageAOn() {
+	document.getElementById("imageA").style.opacity = "0.5";
+}
+			
+function imageAOff() {
+	document.getElementById("imageA").style.opacity = "0";
+}
+
+function imageSOn() {
+	document.getElementById("imageS").style.opacity = "0.5";
+}
+			
+function imageSOff() {
+	document.getElementById("imageS").style.opacity = "0";
+}
+
+function imageYOn() {
+	document.getElementById("imageY").style.opacity = "0.5";
+}
+			
+function imageYOff() {
+	document.getElementById("imageY").style.opacity = "0";
+}
